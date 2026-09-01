8f23648486f418aada4acb0c2493fa3b4b31e607
Alexander Walter <alexander.walter@betterask.erni>
2026-09-01 13:33:46 +0000

    feat: side menu and views

--- src/log.rs
+++ src/log.rs
@@ -3,7 +3,7 @@ use core::fmt::Write;
 use critical_section::Mutex;
 use log::{LevelFilter, Metadata, Record};
 
-const MAX_LOG_SIZE: usize = 4096;
+const MAX_LOG_SIZE: usize = 2048;
 
 struct LogStore {
     buffer: [u8; MAX_LOG_SIZE],
