316c6a4b5654ca3828392246d5315d17c853c9f0
intelligenceconnector <intelligenceconnector@gmail.com>
2026-09-10 17:15:19 +0200

    PR19: enforce explicit unsafe operations

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![deny(unsafe_op_in_unsafe_fn)]
 #![deny(
     clippy::mem_forget,
     reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
