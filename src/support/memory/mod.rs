65eb649effb00b06529df12196cae5b83a9c2580
intelligenceconnector <intelligenceconnector@gmail.com>
2026-09-10 16:49:05 +0200

    PR15: make memory facade explicit

--- src/support/memory/mod.rs
+++ src/support/memory/mod.rs
@@ -1,4 +1,11 @@
-pub(crate) mod data_plane;
 mod monitor;
+mod psram;
+mod stack;
+pub(crate) mod storage;
 
-pub(crate) use monitor::*;
+pub(crate) use monitor::{HeapMonitor, report};
+pub(crate) use psram::enable as enable_psram;
+pub(crate) use stack::{
+    cpu1_stack_monitor_task, init_cpu0_stack_watermark, init_cpu1_stack_watermark,
+    register_cpu1_stack,
+};
