b6b8559b2fc5f667cac07eb9f8f7b218015eb5b8
intelligenceconnector <intelligenceconnector@gmail.com>
2026-09-11 12:30:19 +0200

    PR1: compile app host only for enabled applications

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,2 +1,20 @@
+#[cfg(any(
+    feature = "imu-worldview",
+    feature = "mic-waveform",
+    feature = "speaker-synth",
+    feature = "network-demo",
+    feature = "camera-view",
+    feature = "settings",
+    feature = "log-view",
+))]
 pub(crate) mod model;
+#[cfg(any(
+    feature = "imu-worldview",
+    feature = "mic-waveform",
+    feature = "speaker-synth",
+    feature = "network-demo",
+    feature = "camera-view",
+    feature = "settings",
+    feature = "log-view",
+))]
 pub(crate) mod ui;
