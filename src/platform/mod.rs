9d480bee2852b2f4f68e1f1bedf8c240b0eaff69
intelligenceconnector <intelligenceconnector@gmail.com>
2026-09-12 18:00:07 +0200

    Gate platform modules by capability use

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,2 +1,11 @@
+#[cfg(any(feature = "display", feature = "touch", feature = "camera"))]
 pub(crate) mod board;
+#[cfg(any(
+    feature = "display",
+    feature = "touch",
+    feature = "imu",
+    feature = "mic",
+    feature = "speaker",
+    feature = "camera",
+))]
 pub(crate) mod i2c;
