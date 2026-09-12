8dd97f6f0327807ee0de8b9d7de13e611ac9e2b5
intelligenceconnector <intelligenceconnector@gmail.com>
2026-09-12 17:59:59 +0200

    Compile CPU1 runtime only when needed

--- src/firmware/mod.rs
+++ src/firmware/mod.rs
@@ -1,4 +1,12 @@
 mod bootstrap;
+#[cfg(any(
+    feature = "display",
+    feature = "touch",
+    feature = "imu",
+    feature = "mic",
+    feature = "speaker",
+    feature = "network",
+))]
 mod cpu1;
 
 pub(crate) use bootstrap::{Bootstrap, bootstrap};
