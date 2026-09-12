dd02443e6001877c43696a227dbf6ccb7d85fbab
intelligenceconnector <intelligenceconnector@gmail.com>
2026-09-12 18:01:26 +0200

    Compile diagnostics only for instrumented capabilities

--- src/support/mod.rs
+++ src/support/mod.rs
@@ -1,3 +1,9 @@
+#[cfg(any(
+    feature = "touch",
+    feature = "mic",
+    feature = "speaker",
+    feature = "network",
+))]
 pub(crate) mod diagnostics;
 pub(crate) mod logging;
 pub(crate) mod memory;
